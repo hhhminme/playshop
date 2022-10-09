50b233cbb189848e87bd1d7edb6a01044ac52528
min <hororonoodle@gmail.com>
2022-10-09 23:12:35 +0900

    사진 이미지 최적화

--- constants/constant.ts
+++ constants/constant.ts
@@ -12,42 +12,42 @@ export const TabList = [
 export const ScheduleDayList = [
   [
     {
-      src: '/images/go.svg',
+      src: '/images/go.png',
       title: '강남역 1번 출구 대부도 출발',
       subTitle:
         '오전 11시에 강남역 1번출구에서 버스 탑승이 이루어질 예정입니다. 약 3시간 소요될 예정이며 점심식사가 포함됩니다.',
       time: '10월 13일 오전 11시',
     },
     {
-      src: '/images/walk.svg',
+      src: '/images/walk.png',
       title: '중간 경유지 점심',
       subTitle:
         '콜라비팀으로 20명 예약을 잡아두었어요. 맛있는 생선구이 한정식을 함께 즐겨볼까요?',
       time: '10월 13일 오후 12시 30분',
     },
     {
-      src: '/images/dance.svg',
+      src: '/images/dance.png',
       title: '짐 풀기 및 팀, 숙소 배정',
       subTitle:
         '각자 사용하실 층, 방을 정할 예정이입니다. 그리고 도착 기념 사진 촬영과 플레이샵 시작에 앞서 일정 공유 시간이 있습니다!',
       time: '10월 13일 오후 2시 ~ 3시',
     },
     {
-      src: '/images/finger.svg',
+      src: '/images/finger.png',
       title: '자유 시간!',
       subTitle:
         '수영, 당구, 족구, 노래방 등 원하시는 것을 자유롭게 진행해주시면 됩니다.',
       time: '10월 13일 오후 3시 ~ 4시 30분',
     },
     {
-      src: '/images/play.svg',
+      src: '/images/play.png',
       title: '레크레이션!',
       subTitle:
         '팀별로 다양한 게임을 진행할 예정입니다. 다양한 경품을 받아가실 수 있습니다.',
       time: '10월 13일 오후 6시 30분 ~ 7시',
     },
     {
-      src: '/images/dog.svg',
+      src: '/images/dog.png',
       title: '저녁 식사 및 파티',
       subTitle: '맛있는 고기와 다양한 술, 안주가 준비되어 있습니다.',
       time: '10월 13일 오후 7시',
@@ -55,27 +55,27 @@ export const ScheduleDayList = [
   ],
   [
     {
-      src: '/images/hand.svg',
+      src: '/images/hand.png',
       title: '기상 및 아침 식사',
       subTitle:
         '퇴실 연장이 안되기 때문에 아침에 일어나서 무조건 11시에 나갈 준비를 해야 합니다.',
       time: '10월 14일 오전 8 ~ 9시',
     },
     {
-      src: '/images/girl.svg',
+      src: '/images/girl.png',
       title: '퇴실 준비',
       subTitle: '각자 씻고, 치우고, 청소를 진행할 예정입니다.',
       time: '10월 13일 오후 7시',
     },
     {
-      src: '/images/meditation.svg',
+      src: '/images/meditation.png',
       title: '점심식사',
       subTitle:
         '버스탑승 하시고 가시는 길에 먹을 예정입니다. 전날의 숙취를 해장하는 용도로 국물있는 칼국수로 준비하였습니다. 테이블당 황제칼국수와 해물파전이 준비되어 있습니다.',
       time: '10월 13일 오후 11시 30분',
     },
     {
-      src: '/images/meet.svg',
+      src: '/images/meet.png',
       title: '회사 도착',
       subTitle:
         '모두 고생 많았고 즐거웠습니다. 함께 빠르게 짐 챙겨서 뒷정리 하고 귀가합시다!',
